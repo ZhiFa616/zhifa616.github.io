290ca3fe7175eec9524e4bddc326f401153cc108
ZhiFa616 <linmuxi123@yeah.net>
2026-09-15 16:44:34 +0800

    fix: 修正 fuse 配置并添加搜索页面

--- site.config.ts
+++ site.config.ts
@@ -11,18 +11,26 @@ export default defineSiteConfig({
   },
   description: 'Minecraft Free',
   social: [
-    { name: 'QQ 群', link: 'https://qm.qq.com/q/IfDQEtfkk4', icon: 'i-ri-qq-fill' },
-    { name: 'DeBox', link: 'https://m.debox.pro/card?id=0in7m9t5&invite_code=0in7m9t5', icon: 'i-ri-box-3-line' },
+    {
+      name: 'QQ 群',
+      link: 'https://qm.qq.com/q/IfDQEtfkk4',
+      icon: 'i-ri-qq-fill',
+    },
+    {
+      name: 'DeBox',
+      link: 'https://m.debox.pro/card?id=0in7m9t5&invite_code=0in7m9t5',
+      icon: 'i-ri-box-3-line',
+    },
   ],
   search: {
     enable: true,
     provider: 'fuse',
-    fuse: {
-      options: {
-        keys: ['title', 'tags', 'categories', 'excerpt', 'content'],
-        threshold: 0.6,
-        ignoreLocation: true,
-      },
+  },
+  fuse: {
+    options: {
+      keys: ['title', 'tags', 'categories', 'excerpt', 'content'],
+      threshold: 0.6,
+      ignoreLocation: true,
     },
   },
 } as const)
